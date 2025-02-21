b63ecb07772833349a6fcd5d089d5d986163052d
Juan Esteban Victoria <juane.victoria@uqvirtual.edu.co>
2025-02-20 21:26:24 -0500

    Commit inicial

---
+++ src/test/java/co/edu/uniquindio/seg1/Seg1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.seg1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seg1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
